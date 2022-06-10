8def408f58b85defbeeb9a998e4684794dc5f3ea
Marcelo Machado <mjoelmachadojr@gmail.com>
2022-06-10 19:58:20 -0300

    Input Form
    Created user input form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function App() {
-  return <h1>Olá Mundo!</h1>;
+  return (
+
+    <div className="flex w-full h-screen items-center justify-center">
+
+      <form>
+        <input
+          type="text"
+          placeholder="Cidade"
+          className="p-3 rounded-lg outline-none"
+        />
+        <button
+          type="submit"
+          className="bg-[#A7C7E7] p-3 rounded-lg ml-3 text-[#fff] font-bold"
+        >
+          Pesquisar
+        </button>
+      </form>
+
+    </div>
+  );
 }
 
 export default App;
